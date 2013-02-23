7888a9223dc802d4047dfab8fc81b429cec2cb83
Corey Trombley <corey.trombley33@gmail.com>
2013-02-23 09:32:32 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tubez::Application.config.secret_token = 'ef2170efbb73c0eea6cad79b4a54ce9192f85096eb1da9c292a2b31ac4ae33a55d0177937b9e78e10aaeca7e04f32ab7dfca5fe2463065e593a0853267053013'
